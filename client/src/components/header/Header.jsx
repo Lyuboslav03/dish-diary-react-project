9c3a4290f95a103ce14f77466687544eec980e68
Lyuboslav03 <lyuboslav00@gmail.com>
2023-11-14 19:52:02 +0200

    Added header component.

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+        <header>
+            <p>
+                <a href="#">Dish Diary</a>
+            </p>
+
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">All Recipes</a></li>
+                    <li><a href="#">Add Recipe</a></li>
+                    <li><a href="#">Logout</a></li>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
