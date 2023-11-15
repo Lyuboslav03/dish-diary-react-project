0f9e1e4789b55ba3bcc196a751285dae605c7524
Lyuboslav03 <lyuboslav00@gmail.com>
2023-11-15 16:48:39 +0200

    Added home page component.

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="siteHeroBanner">
+            <div className="container">
+                <h2>Home page</h2>
+            </div>
+        </div>
+    );
+}
