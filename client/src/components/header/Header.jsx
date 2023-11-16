20e6e91859e25ab6f7f3a6438ec109ccef810938
Lyuboslav03 <lyuboslav00@gmail.com>
2023-11-16 20:10:59 +0200

    Updated and modified header component.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,20 +3,22 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <header className={styles.siteHeader}>
-            <h1 className={styles.siteTitle}>
-                <a href="#">Dish Diary</a>
-            </h1>
+            <div className={styles.headerContainer}>
+                <h1 className={styles.siteTitle}>
+                    <a href="#">Dish Diary</a>
+                </h1>
 
-            <nav className={styles.siteNav}>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">All Recipes</a></li>
-                    <li><a href="#">Add Recipe</a></li>
-                    <li><a href="#">Logout</a></li>
-                    <li><a href="#">Login</a></li>
-                    <li><a href="#">Register</a></li>
-                </ul>
-            </nav>
+                <nav className={styles.siteNav}>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">All Recipes</a></li>
+                        <li><a href="#">Add Recipe</a></li>
+                        <li><a href="#">Logout</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
