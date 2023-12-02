46415aa9fac7a3c4d33d5e6499268f0c7a79e317
Lyuboslav03 <lyuboslav00@gmail.com>
2023-12-02 18:08:48 +0200

    Updated home page and added stylization.

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,8 +1,17 @@
+import styles from "./Home.module.css";
+
 export default function Home() {
     return (
-        <div className="siteHeroBanner">
-            <div className="container">
-                <h2>Home page</h2>
+        <div className={styles.homePage}>
+            <div className={styles.container}>
+                <div className={styles.welcomeMessage}>
+                    <h2>Welcome to Dish Diary</h2>
+                    <img src="./images/home-logo.png" />
+                    <h3>#1 Recipe Source for mouth-watering homemade goodies!</h3>
+                    <div className={styles.btnContainer}>
+                        <a href="/all-recipes" className={styles.recipesBtn}>View all recipes</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
