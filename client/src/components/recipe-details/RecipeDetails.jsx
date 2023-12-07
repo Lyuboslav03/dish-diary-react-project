93793bb345e6ddb4a56bd798422f14f09da4781e
Lyuboslav03 <lyuboslav00@gmail.com>
2023-12-07 05:43:35 +0200

    Updated recipe details component.

--- client/src/components/recipe-details/RecipeDetails.jsx
+++ client/src/components/recipe-details/RecipeDetails.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
 import * as recipesService from "../../services/recipesService";
+import AuthContext from "../../contexts/authContext";
 
 import styles from "./RecipeDetails.module.css";
 
 export default function RecipeDetails() {
     const [recipe, setRecipe] = useState({});
     const { recipeId } = useParams();
+    const { id } = useContext(AuthContext);
 
     useEffect(() => {
         recipesService.getOneRecipe(recipeId)
@@ -21,12 +23,18 @@ export default function RecipeDetails() {
                     <img src={recipe.img} />
                 </div>
                 <h3>{recipe.name}</h3>
+                {id === recipe._ownerId && (
+                    <div className={styles.btns}>
+                        <Link to={`/all-recipes/${recipeId}/edit-recipe`}>Edit</Link>
+                        <Link to="">Delete</Link>
+                    </div>
+                )}
             </div>
             <div className={styles.recipeInfo}>
                 <h4>Ingredients:</h4>
                 <div className={styles.ingredients}>
                     <ul>
-                        {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
+                        {recipe.ingredients && recipe.ingredients.split(", ").map((ingredient, index) => (
                             <li key={index}>{ingredient}</li>
                         ))}
                     </ul>
@@ -34,7 +42,7 @@ export default function RecipeDetails() {
                 <h4>Steps:</h4>
                 <div className={styles.steps}>
                     <ol>
-                        {recipe.steps && recipe.steps.map((step, index) => (
+                        {recipe.steps && recipe.steps.split(", ").map((step, index) => (
                             <li key={index}>{step}</li>
                         ))}
                     </ol>
