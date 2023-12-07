edd997d8c855423a4403c89ae63b1354078adf00
Lyuboslav03 <lyuboslav00@gmail.com>
2023-12-07 05:42:01 +0200

    Added add recipe component and styled it.

---
+++ client/src/components/add-recipe/AddRecipe.jsx
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router-dom";
+
+import useForm from "../../hooks/useForm";
+import * as recipesService from "../../services/recipesService";
+
+import styles from "./AddRecipe.module.css";
+
+export default function AddRecipe() {
+    const navigate = useNavigate();
+    
+    const createRecipeSubmitHandler = async (values) => {       
+        const data = {
+            name: values.name,
+            img: values.img,
+            ingredients: values.ingredients,
+            steps: values.steps.replace("\n", ",")
+        }
+        
+        try {
+            await recipesService.createRecipe(data);
+
+            navigate("/all-recipes");
+
+        } catch (error) {
+            console.error(error.message);
+            throw error;
+        }
+    };
+
+    const { values, errors, onChange, onSubmit } = useForm(createRecipeSubmitHandler, {
+        name: '',
+        img: '',
+        ingredients: '',
+        steps: '',
+    })
+    
+    return (
+        <div className={styles.recipeInfo}>
+            <form onSubmit={onSubmit}>
+                <h2>Add Recipe</h2>
+
+                <label htmlFor="name">Recipe Name:</label>
+                <input type="text" id="name" name="name" onChange={onChange} value={values.name} />
+
+                <label htmlFor="img">Image url:</label>
+                <input type="text" id="img" name="img" onChange={onChange} value={values.img} />
+
+                <label htmlFor="ingredients">Ingredients:</label>
+                <textarea name="ingredients" id="ingredients" onChange={onChange} value={values.ingredients} placeholder="Enter ingredients separated by comma..."></textarea>
+
+                <label htmlFor="steps">Steps:</label>
+                <textarea name="steps" id="steps" onChange={onChange} value={values.steps} placeholder="Enter steps separated by comma..."></textarea>
+                {errors && <p>{errors.steps}</p>}
+
+                <input type="submit" className={styles.btnSubmit} value="Add Recipe" />
+            </form>
+        </div>
+    );
+
+}
