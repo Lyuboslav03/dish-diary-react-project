e709924a99ad692175dbfe7689224f24c37415a0
Lyuboslav03 <lyuboslav00@gmail.com>
2023-12-08 22:45:23 +0200

    Updadet add recipe component.

--- client/src/components/add-recipe/AddRecipe.jsx
+++ client/src/components/add-recipe/AddRecipe.jsx
@@ -1,55 +1,85 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import useForm from "../../hooks/useForm";
 import * as recipesService from "../../services/recipesService";
+import { validateInputs } from "../../utils/validateFieldsUtil";
 
 import styles from "./AddRecipe.module.css";
 
 export default function AddRecipe() {
-    const navigate = useNavigate();
-
-    const createRecipeSubmitHandler = async (values) => {
-        const data = {
-            name: values.name,
-            img: values.img,
-            ingredients: values.ingredients.split("\n"),
-            steps: values.steps.split("\n"),
-        }
-
-        try {
-            await recipesService.createRecipe(data);
-
-            navigate("/all-recipes");
-
-        } catch (error) {
-            console.error(error.message);
-            throw error;
-        }
-    };
-
-    const { values, onChange, onSubmit } = useForm(createRecipeSubmitHandler, {
+    const [recipe, setRecipe] = useState({
         name: '',
         img: '',
         ingredients: '',
         steps: '',
-    })
+    });
+    const [inputErrors, setInputErrors] = useState({});
+    
+    const navigate = useNavigate();
+
+    const onChange = (e) => {
+        setRecipe(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+        
+        setInputErrors(state => ({
+            ...state,
+            [e.target.name]: null
+        }));
+    }
+
+    const createRecipeSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        const values = Object.fromEntries(new FormData(e.currentTarget));
+
+        const errors = validateInputs(values);
+
+        if (Object.keys(errors).length === 0) {
+            const data = {
+                name: values.name,
+                img: values.img,
+                ingredients: values.ingredients.split("\n"),
+                steps: values.steps.split("\n"),
+            }
+    
+            try {
+                await recipesService.createRecipe(data);
+    
+                navigate("/all-recipes");
+    
+            } catch (error) {
+                console.error(error.message);
+                throw error;
+            }
+        
+        } else {
+            setInputErrors(errors);
+        }
+        
+    };
 
     return (
         <div className={styles.recipeInfo}>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={createRecipeSubmitHandler}>
                 <h2>Add Recipe</h2>
 
                 <label htmlFor="name">Recipe Name:</label>
-                <input type="text" id="name" name="name" onChange={onChange} value={values.name} />
+                <input type="text" id="name" name="name" onChange={onChange} value={recipe.name} />
+                {inputErrors && <p>{inputErrors.name}</p>}
 
                 <label htmlFor="img">Image url:</label>
-                <input type="text" id="img" name="img" onChange={onChange} value={values.img} />
+                <input type="text" id="img" name="img" onChange={onChange} value={recipe.img} />
+                {inputErrors && <p>{inputErrors.img}</p>}
 
                 <label htmlFor="ingredients">Ingredients:</label>
-                <textarea name="ingredients" id="ingredients" onChange={onChange} value={values.ingredients} placeholder="Enter ingredients separated by comma..."></textarea>
+                <textarea name="ingredients" id="ingredients" onChange={onChange} value={recipe.ingredients} placeholder="Enter ingredients separated by comma..."></textarea>
+                {inputErrors && <p>{inputErrors.ingredients}</p>}
 
                 <label htmlFor="steps">Steps:</label>
-                <textarea name="steps" id="steps" onChange={onChange} value={values.steps} placeholder="Enter steps separated by comma..."></textarea>
+                <textarea name="steps" id="steps" onChange={onChange} value={recipe.steps} placeholder="Enter steps separated by comma..."></textarea>
+                {inputErrors && <p>{inputErrors.steps}</p>}
 
                 <input type="submit" className={styles.btnSubmit} value="Add Recipe" />
             </form>
